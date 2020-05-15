4598d9e47a9b49b37226eba3b8581a5c7645cc66
Chris Padilla <cdpadilla42@gmail.com>
2020-05-15 12:13:00 -0500

    Initial commit

---
+++ src/components/TheaterPicker.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class TheaterPicker extends Component {
+  theaterNameRef = React.createRef();
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    // store theater name
+    const theaterName = this.theaterNameRef.current.value;
+    // TODO visit store at address - react router
+    console.log(theaterName);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleFormSubmit} className="theater-picker">
+        <h1>Chose your theater</h1>
+        <input type="text" name="theater-name" ref={this.theaterNameRef} />
+        <button type="submit">Visit Theater Page</button>
+      </form>
+    );
+  }
+}
+
+export default TheaterPicker;
