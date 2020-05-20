b7a5b7527a5f8fe3e5fed3d6e376eb1b21700bee
Chris Padilla <cdpadilla42@gmail.com>
2020-05-20 10:14:42 -0500

    Calculate ticket order total

--- src/components/Order.js
+++ src/components/Order.js
@@ -8,27 +8,48 @@ class Order extends Component {
     deleteFromOrder: PropTypes.func,
   };
 
-  render() {
+  renderOrderItem = (key) => {
     const order = this.props.order;
     const movies = this.props.movies;
+    const movie = movies[key];
+    if (!movie) return null;
+    return (
+      <p key={key}>
+        {`${order[key]} tickets for ${movie.name}`}
+        <button
+          onClick={() => this.props.deleteFromOrder(key)}
+          className="delete-order-item"
+        >
+          &times;
+        </button>
+      </p>
+    );
+  };
+
+  render() {
+    const movies = this.props.movies;
+    const order = this.props.order;
+    const keys = Object.keys(order);
+    const total = keys.reduce((prevTotal, key) => {
+      if (!movies[key]) return null;
+      // multiply number of tickets by price
+      const moviePrice = movies[key].price;
+      const movieTotal = order[key] * moviePrice;
+      // add to prev total
+      return prevTotal + movieTotal;
+    }, 0);
     return (
       <div>
         <h2>Here's your tix 🎟</h2>
         {Object.keys(order).map((key) => {
-          const movie = movies[key];
-          if (!movie) return null;
-          return (
-            <p key={key}>
-              {`${order[key]} tickets for ${movie.name}`}
-              <button
-                onClick={() => this.props.deleteFromOrder(key)}
-                className="delete-order-item"
-              >
-                &times;
-              </button>
-            </p>
-          );
+          return this.renderOrderItem(key);
         })}
+        <div className="total">
+          <p>
+            Total:
+            <span>{total}</span>
+          </p>
+        </div>
       </div>
     );
   }
