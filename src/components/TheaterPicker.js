6e0ba61989bb37b9b8ec3d0d757c17d041d492ba
Chris Padilla <cdpadilla42@gmail.com>
2020-05-21 16:33:24 -0500

    stylize theater picker

--- src/components/TheaterPicker.js
+++ src/components/TheaterPicker.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getStreetName } from '../helper';
+import '../css/theaterPicker.css';
 
 class TheaterPicker extends Component {
   theaterNameRef = React.createRef();
@@ -15,13 +16,15 @@ class TheaterPicker extends Component {
     return (
       <form onSubmit={this.handleFormSubmit} className="theater-picker">
         <h1>Chose your theater</h1>
-        <input
-          type="text"
-          name="theater-name"
-          ref={this.theaterNameRef}
-          value={getStreetName()}
-        />
-        <button type="submit">Visit Theater Page</button>
+        <div className="picker">
+          <input
+            type="text"
+            name="theater-name"
+            ref={this.theaterNameRef}
+            value={getStreetName()}
+          />
+          <button type="submit">Visit Theater Page</button>
+        </div>
       </form>
     );
   }
